0680af7add69ad8a7f7738df6632ee1b05567265
Nelson Neto <nhsneto@gmail.com>
2026-09-16 15:42:38 -0300

    refactor: utiliza nvm para instalacao do nodejs em prod

--- vagrant/scripts/setup-node.sh
+++ vagrant/scripts/setup-node.sh
@@ -1,8 +1,10 @@
-apt-get update
-
-apt-get install -y nodejs npm
-
-node -v
-npm -v
+# Excecuta a instalacao do Node.js como usuario vagrant ao inves de root
+sudo -u vagrant -i bash -c '
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash;
+source "$HOME/.nvm/nvm.sh";
+nvm install 24;
+node -v;
+npm -v;
+'
 
 echo "Provisionamento do Node.js concluído"
